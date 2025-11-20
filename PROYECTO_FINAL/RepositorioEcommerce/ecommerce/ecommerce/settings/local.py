5110aaa276c93bcdeaec4bbdc7d26423557c34f3
Nicolas <nicotortosa@gmail.com>
2025-11-20 12:15:14 -0300

    intro a templates

--- PROYECTO_FINAL/RepositorioEcommerce/ecommerce/ecommerce/settings/local.py
+++ PROYECTO_FINAL/RepositorioEcommerce/ecommerce/ecommerce/settings/local.py
@@ -1,1 +1,15 @@
 from .base import *
+
+SECRET_KEY = 'django-insecure-yuhihp2taa46(i_di^znj=m7cd(dek(y#rr0n+=9n!w^ver%o9'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
